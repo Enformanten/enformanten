5f219caa46373d13bbd5d09f8f11bc39a0f38775
Nicolai Thomsen <nicolaibthomsen@hotmail.dk>
2023-10-05 14:20:06 +0200

    docks template, cleaned flow

--- ai/tilly/services/ml/__init__.py
+++ ai/tilly/services/ml/__init__.py
@@ -1,1 +1,6 @@
 from tilly.services.ml.transformations import Transformer  # noqa
+from tilly.services.ml.model_registry import (
+    ModelRegistry,  # noqa
+    update_registry,  # noqa
+    get_current_registry,  # noqa
+)  # noqa
